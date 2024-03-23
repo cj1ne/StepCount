800389628c4440d6da115c81c8b45d9e813c50aa
Jongwon Choi <cjw940402@gmail.com>
2024-03-23 14:48:15 +0900

    걸음 수 위젯 추가

---
+++ app/src/main/kotlin/com/glen/stepcount/ui/widget/StepCountAppWidget.kt
@@ -0,0 +1,57 @@
+package com.glen.stepcount.ui.widget
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+import com.glen.stepcount.R
+import com.glen.stepcount.domain.GetTodayStepsRecordUseCase
+import com.glen.stepcount.model.StepsRecord
+import com.glen.stepcount.ui.main.MainActivity
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class StepCountAppWidget : AppWidgetProvider() {
+
+    @Inject
+    lateinit var getTodayStepsRecordUseCase: GetTodayStepsRecordUseCase
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        val stepsRecord = getTodayStepsRecordUseCase()
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId, stepsRecord)
+        }
+    }
+}
+
+fun updateAppWidget(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int,
+    stepsRecord: StepsRecord
+) {
+    val pendingIntent = PendingIntent.getActivity(
+        context,
+        0,
+        Intent(context, MainActivity::class.java),
+        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+    )
+    val views = RemoteViews(context.packageName, R.layout.step_count_app_widget).apply {
+        setTextViewText(
+            R.id.step_count,
+            when (stepsRecord) {
+                is StepsRecord.Available -> stepsRecord.count.toString()
+                is StepsRecord.Unavailable -> "?"
+            }
+        )
+        setOnClickPendingIntent(android.R.id.background, pendingIntent)
+    }
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
